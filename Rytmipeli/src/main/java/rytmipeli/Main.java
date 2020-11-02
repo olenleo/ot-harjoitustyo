92a0de8e59da79dd12eafd1c954387f4abc85b0c
Leo Niemi <niemi.leo@gmail.com>
2020-11-02 18:00:24 +0200

    rytmipelin main

--- Rytmipeli/src/main/java/rytmipeli/Main.java
+++ Rytmipeli/src/main/java/rytmipeli/Main.java
@@ -6,10 +6,11 @@ package rytmipeli;
  *
  * @author olenleo (Leo Niemi)
  */
+import sovelluslogiikka.SovellusLogiikka;
 public class Main {
 
     public static void main(String args[]) {
-        
+        SovellusLogiikka s = new SovellusLogiikka();
     }
 
 }
