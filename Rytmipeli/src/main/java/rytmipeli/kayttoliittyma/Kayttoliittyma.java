bbe52326edd091c3dde87b13fc6f6456e4c236a5
Leo Niemi <niemi.leo@gmail.com>
2020-11-20 12:30:08 +0200

    Lisätty napit ja CSS:ää, TODO: Muokkaa värit paikalleen

--- Rytmipeli/src/main/java/rytmipeli/kayttoliittyma/Kayttoliittyma.java
+++ Rytmipeli/src/main/java/rytmipeli/kayttoliittyma/Kayttoliittyma.java
@@ -29,40 +29,76 @@ public class Kayttoliittyma extends Application {
         Canvas canvas = new Canvas();
         canvas.setHeight(160);
         canvas.setWidth(800);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        gc.setFill(Color.DARKGREY);
+        gc.fillRect(0, 0, 800, 160);
         Scene scene = new Scene(new Group());
         scene.getStylesheets().add("test.css");
-
-        String greenCSS = "-fx-background-color: linear-gradient(#b8ee36, #80c800),"
+        String redCSS = "-fx-background-color: linear-gradient(#FF8830, #D55A00),"
                 + "radial-gradient(center 50% -40%, "
                 + "radius 200%, "
-                + "#b8ee36 45%,"
+                + "#FF3200 45%, " // yläosa
+                + "#FF6D07 50%); " // alaosa
+                + "-fx-background-radius: 2, 20;    "
+                + "-fx-background-insets: 25, 10;"
+                + "-fx-effect: dropshadow( three-pass-box, rgba(0,0,0,0.9),5,0.0,0,1);"
+                + "-fx-text-fill: #395306;";
+
+        String yellowCSS = "-fx-background-color: linear-gradient(#FF8830, #D55A00),"
+                + "radial-gradient(center 50% -40%, "
+                + "radius 200%, "
+                + "#FFB200 45%, " // yläosa
+                + "#FF6D07 50%); " // alaosa
+                + "-fx-background-radius: 2, 20;    "
+                + "-fx-background-insets: 25, 10;"
+                + "-fx-effect: dropshadow( three-pass-box, rgba(0,0,0,0.9),5,0.0,0,1);"
+                + "-fx-text-fill: #395306;";
+
+        String limeCSS = "-fx-background-color: linear-gradient(#b8ee36, #80c800),"
+                + "radial-gradient(center 50% -40%, "
+                + "radius 200%, "
+                + "#CDFF00 45%,"
                 + "#80c800 50%); "
                 + "-fx-background-radius: 2, 20;"
                 + "-fx-background-insets: 25, 10;"
                 + "-fx-effect: dropshadow( three-pass-box, rgba(0,0,0,0.4),5,0.0,0,1);"
                 + "-fx-text-fill: #395306;";
-        String redCSS = "-fx-background-color: linear-gradient(#FF8830, #D55A00),"
+        
+        String greenCSS = "-fx-background-color: linear-gradient(#b8ee36, #80c800),"
                 + "radial-gradient(center 50% -40%, "
                 + "radius 200%, "
-                + "#FF8830 45%, " // yläosa
-                + "#D55A00 50%); " // alaosa
-                + "-fx-background-radius: 2, 20;    "
+                + "#4EFF00 45%,"
+                + "#80c800 50%); "
+                + "-fx-background-radius: 2, 20;"
                 + "-fx-background-insets: 25, 10;"
-                + "-fx-effect: dropshadow( three-pass-box, rgba(0,0,0,0.9),5,0.0,0,1);"
+                + "-fx-effect: dropshadow( three-pass-box, rgba(0,0,0,0.4),5,0.0,0,1);"
                 + "-fx-text-fill: #395306;";
 
         Insets buttonInset = new Insets(10, 10, 10, 10);
 
         Button tahtiButton = new Button("[1/4]");
         tahtiButton.setMinSize(160, 160);
-        tahtiButton.setStyle(greenCSS);
+        tahtiButton.setStyle(redCSS);
         tahtiButton.setPadding(buttonInset);
+        
         Button nextButton = new Button("SKIP");
         nextButton.setMinSize(160, 160);
-        nextButton.setStyle(redCSS);
+        nextButton.setStyle(yellowCSS);
         nextButton.setPadding(buttonInset);
+        
+        Button laattaButton = new Button("7");
+        laattaButton.setMinSize(160, 160);
+        laattaButton.setStyle(limeCSS);
+        laattaButton.setPadding(buttonInset);
+        
+        Button molemmatButton = new Button("*");
+        molemmatButton.setMinSize(160, 160);
+        molemmatButton.setStyle(greenCSS);
+        molemmatButton.setPadding(buttonInset);
 
-        HBox buttons = new HBox(tahtiButton, nextButton);
+
+        HBox buttons = new HBox(tahtiButton, nextButton, laattaButton, molemmatButton);
         buttons.setPadding(new Insets(1, 1, 1, 1));
         ((Group) scene.getRoot()).getChildren().add(canvas);
         ((Group) scene.getRoot()).getChildren().add(buttons);
