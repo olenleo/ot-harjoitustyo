416481e4499ec5efb6c955f8d2f9d23a5b329faa
Leo Niemi <niemi.leo@gmail.com>
2020-12-08 14:08:28 +0200

    uusi kaavio

---
+++ Rytmipeli/src/main/java/rytmipeli/pisteet/PisteToiminnalllisuus.java
@@ -0,0 +1,14 @@
+
+package rytmipeli.pisteet;
+
+import java.io.File;
+import java.io.FileReader;
+
+public class PisteToiminnalllisuus {
+    
+    private FileReader fr;
+    public PisteToiminnalllisuus() {
+
+        
+    }
+}
